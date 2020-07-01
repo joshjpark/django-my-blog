80200af462ba4b310420532ebd61d0a63a553624
joshjpark <joshua-explorer@hotmail.com>
2020-07-01 14:21:22 -0700

    Create blog post template update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
 def home(request):
     context = {
@@ -9,17 +9,23 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
-# list of posts
+# list of posts view
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
-# individual post
+# individual post view
 class PostDetailView(DetailView):
     model = Post
-     
+
+# individual post create view
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+#  
 
 
 def about(request):
