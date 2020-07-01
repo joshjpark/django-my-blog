301d20f814b7aac17aa2095c7b95ee6d218c7ad9
joshjpark <joshua-explorer@hotmail.com>
2020-06-30 23:36:47 -0700

    Changeable profile page

--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
 
@@ -19,4 +19,11 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'Users/profile.html')
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
+    return render(request, 'Users/profile.html', context)
