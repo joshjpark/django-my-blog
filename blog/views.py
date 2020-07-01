ebe543d5f5a49a6279c92fbc82b1f1c0f847fded
joshjpark <joshua-explorer@hotmail.com>
2020-07-01 14:12:07 -0700

    Page for specific post added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 def home(request):
     context = {
@@ -9,12 +9,18 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+# list of posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    
+
+# individual post
+class PostDetailView(DetailView):
+    model = Post
+     
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title' : 'About'})
