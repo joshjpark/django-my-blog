d456cbbc451d1af111a92f55a921864a63f0216a
joshjpark <joshua-explorer@hotmail.com>
2020-07-01 20:52:38 -0700

    Scaffold code for email / password reset

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
     path('logout/', auth_views.LoginView.as_view(template_name='Users/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='Users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='Users/password_reset_done.html'), name='password_reset_done'),
     path('profile/', user_views.profile, name='profile')
 ]
 
