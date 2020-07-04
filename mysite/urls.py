ad8393750a5fd0b03b1932979e7d34a0ddbf50f3
joshjpark <joshua-explorer@hotmail.com>
2020-07-04 01:14:51 -0700

    Fixed minor typo in route

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
     path('logout/', auth_views.LoginView.as_view(template_name='Users/logout.html'), name='logout'),
